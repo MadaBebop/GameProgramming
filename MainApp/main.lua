af93918b0152f588675f7803eae8ba3a163e9207
MadaBebop <45188583+MadaBebop@users.noreply.github.com>
2021-01-12 10:56:30 +0100

    Add files via upload

---
+++ MainApp/main.lua
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------------------
+-- MAIN LUA:Full Metal Corona
+-- Progetto uniUd
+-- Corso di game programming, Stm uniud.
+--
+-----------------------------------------------------------------------------------------
+
+-- Nasconde la barra di stato del telefono
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Libreria composer
+local composer = require "composer"
+
+-- Inizio con caricamento del menù
+composer.gotoScene( "scene.menu" )   --
+-- composer.gotoScene ( "defeat" ) --
+-- composer.gotoScene ( "victory" )

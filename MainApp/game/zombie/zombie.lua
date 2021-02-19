5d5eabae89f9b49db93cda3044d8994015de1118
Cristian Murtas <skylerhunter6@gmail.com>
2021-02-19 15:28:30 +0100

    Minor changes

--- MainApp/game/zombie/zombie.lua
+++ MainApp/game/zombie/zombie.lua
@@ -3,14 +3,14 @@ local M = {}
 function M.createZombie()
     -- idle sheet
     local idleData = {width = 65, height = 78, numFrames = 15, sheetContentWidth = 975, sheetContentHeight = 78}
-    local idleSheet = graphics.newImageSheet("robotfree/zombie/IdleSheet.png", idleData)
+    local idleSheet = graphics.newImageSheet("game/zombie/IdleSheet.png", idleData)
 
     -- walk sheet
     local walkData = {width = 65, height = 78, numFrames = 10, sheetContentWidth = 650, sheetContentHeight = 78}
-    local walkSheet = graphics.newImageSheet("robotfree/zombie/walkSheet.png", walkData)
+    local walkSheet = graphics.newImageSheet("game/zombie/walkSheet.png", walkData)
 
     local deathData = {width = 63, height = 53, numFrames = 12, sheetContentWidth = 756, sheetContentHeight = 53}
-    local deathSheet = graphics.newImageSheet('robotfree/zombie/zombieDeathSheet.png', deathData)
+    local deathSheet = graphics.newImageSheet('game/zombie/zombieDeathSheet.png', deathData)
 
 
     local zombieSequences = {
@@ -44,7 +44,7 @@ function M.createZombie()
     }
 
     local zombie = display.newSprite(idleSheet, zombieSequences)
-    local type = 'zombie'
+    zombie.type = 'zombie'
     local zombieShape = {}
     physics.addBody(zombie, 'dynamic', {friction = 1.0, density = 3.5})
     zombie:scale(-1, 1)

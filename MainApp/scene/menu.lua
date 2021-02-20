a754952b26a866150e2ebfdf42f1ee2b569de363
MadaBebop <45188583+MadaBebop@users.noreply.github.com>
2021-02-20 12:01:07 +0100

    Inserito lo slider con controllo del volume
    Ancora non riesco a eliminare l'audio nel cambio di scena (da menù a livello1)
    forse anzi che removeSelf devo usare semplicemente .pause?

--- MainApp/scene/menu.lua
+++ MainApp/scene/menu.lua
@@ -7,38 +7,44 @@
 --------------------------------------------
 --Utilizzo della libreria composer con annessa creazione della scena
 local composer = require "composer"
-
-local scene = composer.newScene()
-
--- Utilizzo della libreria widget
-local widget = require "widget"
+local widget = require "widget" -- widget library per il bottone di avvio e lo slider
 
 --------------------------------------------
 -- Dichiarazioni locali
 --------------------------------------------
+-- creazione nuova scena composer
+local scene = composer.newScene()
+
+---bottone e suo ascoltatore
 local playBtn -- Creazione del bottone che darà inizio al livello 1
-
-
--- Ascoltatore del bottone (sulla pressione)
+-- Ascoltatore del bottone
 local function onPlayBtnRelease()
-			composer.gotoScene( "scene.livello2")
-end -- Fine funzione bottone
+			composer.gotoScene( "scene.livello1")
+end
+
+--- slider e suo ascoltatore
+local slider -- creazione dello slider
+-- Ascoltatore dello slider
+local function sliderListener( event )
+		audio.setVolume(event.value/100)
+end
 
 ---------
---- CREATE
+--- fase CREATE
 --------
 function scene:create( event )
 	local sceneGroup = self.view -- Chiamata quando la scena non esiste
 
-
-	-- background ATTENZIONE NECESSITA SFONDO
+	-- background
 	local background = display.newImageRect( "scene/menusrc/background.png", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x = 0 + display.screenOriginX
 	background.y = 0 + display.screenOriginY
 
+	---
 	-- Creazione del widget bottone (Il quale carichera il gioco)
+	---
 	playBtn = widget.newButton{ --creazione delle caratteristiche del bottone
 		labelColor = { default={ 1.0 }, over={ 0.5 } }, -- colore dell'etichetta
 		defaultFile = "scene/menusrc/button.png",   --
@@ -49,11 +55,35 @@ function scene:create( event )
 
 	playBtn.x = display.contentCenterX  --Posizionamento del bottone x,y
 	playBtn.y = display.contentHeight - 125
+	------
+	-- Fine Bottone
+	------
+
+	-----
+	-- Slider
+	-----
+	slider = widget.newSlider( -- definizione delle caratteristiche dello slider
+    {
+        x = 500, -- posizionamento
+        y = 120,
+				orientation = "vertical",
+        height = 100,
+        value = 50,  -- Start slider at 50% (optional)
+        listener = sliderListener
+    } )
+	---------------
+	--- fine slider
+	---------------
+
+	--Inserimento della musica
+	local music = audio.loadStream("music/WBA Free Track.mp3")
+	audio.setVolume(0.5)
+	audio.play(music,{loops=-1})
 
 	-- Inserimento GRUPPI delle scene
 	sceneGroup:insert( background ) --Sfondo
 	sceneGroup:insert( playBtn )  --Bottone
-
+	sceneGroup:insert( slider ) -- slider
 
 end -- Fine della creazione degli oggetti
 
@@ -69,11 +99,7 @@ function scene:show( event )
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		--
 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
 	end
 end
 
@@ -102,19 +128,18 @@ end
 function scene:destroy( event )
 	local sceneGroup = self.view
 
-	-- Called prior to the removal of scene's "view" (sceneGroup)
 	background:removeSelf()
 	background = nil
 	composer.removeScene('menu')
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
 
 	if playBtn then
 		playBtn:removeSelf()	-- I widget devono essere eliminati manualmente
 		playBtn = nil
 	end
-end
 
+	slider:removeSelf( )
+	
+end
 ---------------------------------------------------------------------------------
 -- ASCOLTATORI della scena
 ---------------------------------------------------------------------------------

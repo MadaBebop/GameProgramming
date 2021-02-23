26033802bec82f88645a2c2ce4648fac7fce2d2e
Cristian Murtas <skylerhunter6@gmail.com>
2021-02-23 18:34:36 +0100

    Creazione di modulo door

--- MainApp/game/lib/door.lua
+++ MainApp/game/lib/door.lua
@@ -4,20 +4,29 @@ local composer = require 'composer'
 
 function M.createDoor()
 
-    local scene = composer.getScene(composer.getSceneNAme('current'))
+    local door = display.newRect(20,20 , 20,20 , 20,20 , 20,20)
+    physics.addBody(door, 'static', {isSensor = true})
+	door:setFillColor( 0.5 )
+	-- door.isVisible = false
+	door.type = 'door'
+
+    local scene = composer.getScene(composer.getSceneName('current'))
 
     function collision(event) 
         local phase = event.phase
         local other = event.other
 
         if (phase == 'began') then
-            if (other == 'hero') then 
-                composer.gotoScene('scene.refresh', {params = {
-                    map = self.map
-                }})
+            if (other.type == 'robot') then 
+                print('colliding')
+                composer.gotoScene('scene.cutScene', {params = {map = door.map}})
             end
         end
     end
+
+    door:addEventListener('collision',collision)
+
+    return door
 end
 
 return M

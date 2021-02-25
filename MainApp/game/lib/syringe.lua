0b495313ef8b5c8bdbc08dbc4a2cec80bc4f1ecb
Cristian Murtas <skylerhunter6@gmail.com>
2021-02-25 15:54:22 +0100

    Aggiunto modulo per la siringa

---
+++ MainApp/game/lib/syringe.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+function M.createSyringe() 
+
+    local syringe = display.newImage('scene/maps/lvl2/siringe.png')
+    physics.addBody(syringe, 'static', {bounce = 0.0, isSensor = true})
+    syringe.type = 'syringe'
+    syringe.isPicked = false
+
+    function onCollision( event )
+        local phase = event.phase
+        local other = event.other
+        if (phase == 'began') then
+            if (other.type == 'robot') then
+                syringe.isPicked = true
+                syringe:removeSelf()
+                syringe = nil
+            end
+        end
+    end
+
+    syringe:addEventListener('collision', onCollision)
+
+    return syringe
+
+end
+
+return M

1455cd33dc9c082e45c4705cda66b5a9df6ed5bf
Cristian Murtas <skylerhunter6@gmail.com>
2021-02-26 13:26:49 +0100

    Commenti sul menu

--- MainApp/scene/menu.lua
+++ MainApp/scene/menu.lua
@@ -9,12 +9,10 @@
 local composer = require "composer"
 local widget = require "widget" -- widget library per il bottone di avvio e lo slider
 
---------------------------------------------
--- Dichiarazioni locali
---------------------------------------------
 
 local scene = composer.newScene()
 
+-- Dichiarazioni variabili 
 local background
 local playBtn
 local slider
@@ -22,15 +20,15 @@ local music
 
 
 
-
 -- Funzione per far partire il livello 1 alla pressione del tasto play
 local function onPlayBtnRelease()
 	composer.removeHidden() -- é necessario
-	composer.gotoScene( "scene.livello1", {params = {}})
+	-- composer.gotoScene( "scene.livello1", {params = {}})
+	composer.gotoScene( "scene.livello1")
 end
 
 
--- Ascoltatore dello slider
+-- Ascoltatore dello slider per il volume
 local function sliderListener( event )
 	audio.setVolume(event.value/100)
 end
@@ -41,13 +39,11 @@ end
 function scene:create( event )
 	local sceneGroup = self.view
 
-	-- background
+	-- Caricamento del background del menu
 	background = display.newImageRect( "scene/menusrc/background.png", display.actualContentWidth, display.actualContentHeight )
 
 
-	---
 	-- Creazione del widget bottone (Il quale carichera il gioco)
-	---
 	playBtn = widget.newButton{ --creazione delle caratteristiche del bottone
 		labelColor = { default={ 1.0 }, over={ 0.5 } }, -- colore dell'etichetta
 		defaultFile = "scene/menusrc/button.png",   --
@@ -57,9 +53,8 @@ function scene:create( event )
 	}
 
 
-	-----
-	-- Slider
-	-----
+	
+	-- Creazione dello slider per il controllo del volume
 	slider = widget.newSlider( -- definizione delle caratteristiche dello slider
     {
         x = display.contentWidth - 40, -- posizionamento
@@ -76,10 +71,10 @@ function scene:create( event )
 	audio.setVolume(0.5, {channel = 1}) -- volume std al 50%
 
 
-	-- Inserimento GRUPPI delle scene
-	sceneGroup:insert( background ) --As.Sfondo
-	sceneGroup:insert( playBtn )  --As. Bottone
-	sceneGroup:insert( slider ) --As. Slider
+	-- Inserimento dei display object all'interno del gruppo scena principale
+	sceneGroup:insert( background ) 
+	sceneGroup:insert( playBtn )  
+	sceneGroup:insert( slider ) 
 
 end
 
@@ -99,7 +94,7 @@ function scene:show( event )
 		playBtn.x = display.contentCenterX
 		playBtn.y = display.contentHeight - 125
 	elseif phase == "did" then
-		audio.play(music,{loops=-1, channel = 1}) -- avvio
+		audio.play(music,{loops=-1, channel = 1}) 
 
 	end
 end
@@ -128,8 +123,9 @@ end
 function scene:destroy( event )
 	local sceneGroup = self.view
 
-	if playBtn then
-		playBtn:removeSelf()	-- I widget devono essere eliminati manualmente
+	-- Rimozione dei widget bottone play e slider volume
+	if (playBtn) then
+		playBtn:removeSelf()	
 		playBtn = nil
 	end
 
@@ -137,10 +133,9 @@ function scene:destroy( event )
 	slider = nil
 
 end
----------------------------------------------------------------------------------
--- ASCOLTATORI della scena
----------------------------------------------------------------------------------
 
+
+-- ASCOLTATORI della scena
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
@@ -148,4 +143,4 @@ scene:addEventListener( "destroy", scene )
 
 ---------------------------------------------------------------------------------
 
-return scene  --FINE MENU
+return scene  -- FINE MENU
